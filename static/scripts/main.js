b4aa3f20743af24fd258c0dd512ad961a3cba50f
Owen Patrick Falculan <owenkamote@gmail.com>
2021-08-18 21:09:49 +0800

    update contents

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -5,13 +5,14 @@ function topFunction() {
 
 $(document).ready(function(){
   $.get('https://api.github.com/users/markbirds/repos?per_page=100',function(data){
-    var projects = ["Anime-Recommendation-System","Deep-Learning-Projects","Flask-Chat-Rooms","Machine-Learning-Projects","Natural-Language-Processing-Projects","Youtube-Contents"];
-    var project_name = ["Anime Recommendation System","Deep Learning Projects","Flask Chat Rooms","Machine Learning Projects","Natural Language Processing Projects","Youtube Contents"];
+    var projects = ["Code-Invaders","Deep-Learning-Projects","Flask-Chat-Rooms","Machine-Learning-Projects","Natural-Language-Processing-Projects","Youtube-Contents"];
+    var project_name = ["Code Invaders","Deep Learning Projects","Flask Chat Rooms","Machine Learning Projects","Natural Language Processing Projects","Youtube Contents"];
     var tag_colors = ["primary","success","danger","warning","info","dark","secondary"]
     var counter = 0;
     var cards = "";
     for(var i=0;i<data.length;i++){
-      if(projects.includes(data[i].name)){ 
+      if(projects.includes(data[i].name)){
+        cards += ( counter === 0 || counter === 3 ) ? '<section>' : '';
         cards+=
         '<div class="card-project">'+
           '<div class="card-header">'+ 
@@ -20,14 +21,12 @@ $(document).ready(function(){
           '</div>'+
           '<div class="card-body">'+
             '<p class="description">' + data[i].description + '</p>' +
-            '<div class="source-code">'+
-              '<p><a href="'+ data[i].html_url +'" target="_blank">Click here for source code.</a></p>' +
-            '</div>'+
             '<div class="created">'+
               '<p><i> Created ' + jQuery.timeago(data[i].created_at)  + '</i></p>' +
             '</div>'+
             '</div>' +
         '</div>';
+        cards += ( counter === 2 || counter === 5) ? '</section>' : '';
         counter++;
       }
     }
@@ -38,13 +37,15 @@ $(document).ready(function(){
 var prevScrollpos = window.pageYOffset;
 // When the user scrolls the page, execute myFunction 
 window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("myTopnav").style.top = "0px";
-  } else {
-    document.getElementById("myTopnav").style.top = "-60px";
+  if($('.topnav.responsive').length===0){
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementsByTagName("nav")[0].style.top = "0px";
+    } else {
+      document.getElementsByTagName("nav")[0].style.top = "-60px";
+    }
+    prevScrollpos = currentScrollPos;
   }
-  prevScrollpos = currentScrollPos;
 };
 
 function fadeOut(){
@@ -55,6 +56,16 @@ function fadeOut(){
   },3000);   
   setTimeout(function(){
     main.css("display", "block");
+    $('nav').css("display", "block");
   },4000);  
 } 
 
+function dropLinks(x) {
+  x.classList.toggle("change");
+  let topnav = document.getElementById("myTopnav");
+  if (topnav.className === "topnav") {
+    topnav.className += " responsive";
+  } else {
+    topnav.className = "topnav";
+  }
+}
