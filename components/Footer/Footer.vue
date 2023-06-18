a488e557691d7a8496c0a4e9716f2a2351026fd5
Owen Patrick Falculan <fowenpatrick@gmail.com>
2023-06-18 15:05:34 +0800

    feat: use nuxt

---
+++ components/Footer/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="text-center mt-24 mb-4 text-gray-600">
+    <div class="italic">
+      <a href="/internetdog.gif" target="_blank">
+        On the Internet, nobody knows you're a dog
+      </a>
+    </div>
+    <div class="flex justify-center items-center">
+      <span class="mx-2">Powered by</span>
+      <Icon name="logos:nuxt" size="64"/>
+    </div>
+  </section>
+</template>
+<script setup lang="ts">
+</script>
---
+++ components/Gallery/Gallery.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="mw-full md:w-[80%] lg:w-[60%] mx-4 md:mx-auto md:mt-24 my-16 text-gray-600 cursor-default">
+    <div class="text-xl bg-gradient-to-r from-slate-600 text-slate-100 rounded-lg py-3 px-4 font-bold uppercase">
+      Gallery
+    </div>
+    <GallerySwiper :images="images"/>
+  </section>
+</template>
+<script setup>
+const images = [
+    "/gallery/gallery1.jpg",
+  "/gallery/gallery2.jpg",
+  "/gallery/gallery3.jpg",
+  "/gallery/gallery4.jpg",
+  "/gallery/gallery5.jpg",
+  "/gallery/gallery6.jpg",
+  "/gallery/gallery7.jpg",
+  "/gallery/gallery8.jpg",
+]
+</script>
