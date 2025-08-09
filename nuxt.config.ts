737e73d86cb66ff9f83697c24be42fdb7a1e50cd
Owen Falculan <owen.falculan@firstdigitalfinance.com>
2025-08-09 09:01:59 +0800

    fix: update portfolio description in Nuxt configuration
    - Changed the content of the description meta tag in nuxt.config.ts to include the full name "Owen Patrick Falculan" for improved clarity.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,10 @@ export default defineNuxtConfig({
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { name: "description", content: "Owen Falculan - Personal Portfolio" },
+        {
+          name: "description",
+          content: "Owen Patrick Falculan - Personal Portfolio",
+        },
         { name: "theme-color", content: "#000000" },
       ],
       link: [
